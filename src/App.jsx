6bf25afcf8e4f3b308cfe1218c7a50a6b008a76f
Smit Thakkar <sm928548@dal.ca>
2022-04-24 12:58:31 -0300

    sidebar menu created

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,15 @@ import Projects from "./components/projects/Projects"
 import Contact from "./components/contact/Contact"
 import "./app.scss"
 import { useState } from "react"
+import Menu from "./components/menu/Menu"
+
 
 function App() {
   const [menuOpen,setMenuOpen] = useState(false)
   return (
     <div className="app">
       <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
       <div className="sections">
       <Intro/>
       <Probackground/>
---
+++ src/components/menu/Menu.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import "./menu.scss"
+
+export default function Menu({menuOpen,setMenuOpen}) {
+  return (
+    <div className={"menu " + (menuOpen && "active")}>
+        <ul>
+            <li onClick={()=>setMenuOpen(false)}>
+                <a href="#intro">About me</a>
+            </li>
+            <li onClick={()=>setMenuOpen(false)}>
+                <a href="#probackground">Professional Background</a>
+            </li>
+            <li onClick={()=>setMenuOpen(false)}>
+                <a href="#education">Education</a>
+            </li>
+            <li onClick={()=>setMenuOpen(false)}>
+                <a href="#projects">Projects</a>
+            </li>
+            <li onClick={()=>setMenuOpen(false)}>
+                <a href="#contact">Contact me</a>
+            </li>
+        </ul>
+
+    </div>
+  )
+}
