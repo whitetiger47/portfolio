827421d2a5468970ed89497376fcebc7e36a8e01
Smit Thakkar <sm928548@dal.ca>
2022-05-02 19:33:53 -0300

    dynamic projects list

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import "./intro.scss"
+import { init } from 'ityped'
+
+let isLoading = true;
+
 export default function Intro() {
+
+  const textRef = useRef(null);
+
+  useEffect(()=>{
+
+    if(textRef.current && isLoading) {
+      isLoading=false;
+    
+    init(textRef.current,{
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed:60,
+      
+      strings: ["Developer","Designer","Content Creator"],
+    });
+  }
+
+  },[]);
+
   return (
     <div className="intro" id="intro">
-
+        <div className="left">
+          <div className="imgContainer">
+            <img src="assets/smit.jpg" alt="" />
+          </div>
+        </div>
+        <div className="right">
+          <div className="wrapper">
+            <h2>Hi there, I'm</h2>
+            <h1>Smit Thakkar</h1>
+            <h3>Freelance <span ref ={textRef}></span></h3>
+          </div>
+          <a href="#probackground">
+          <img src="assets/down.png" alt="" />
+          </a>
+        </div>
     </div>
   )
 }
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -4,6 +4,46 @@ import "./projects.scss"
 export default function Projects() {
   return (
     <div className="projects" id="projects">
+      <h1>Projects</h1>
+      <ul>
+        <li className="active">Featured</li>
+        <li>Web App</li>
+        <li>Mobile App</li>
+        <li>Designing</li>
+        <li>Branding</li>
+      </ul>
+      <div className="container">
+        <div className="item">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn: ANd9GcQWZxqJ65p2YnxZ1Ddd5BTo2t1GQ1SFqJvb7jYaf54uNtftW9lMLg" 
+        alt="" />
+        <h3>Banking App</h3>
+        </div>
+        <div className="item">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn: ANd9GcQWZxqJ65p2YnxZ1Ddd5BTo2t1GQ1SFqJvb7jYaf54uNtftW9lMLg" 
+        alt="" />
+        <h3>Banking App</h3>
+        </div>
+        <div className="item">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn: ANd9GcQWZxqJ65p2YnxZ1Ddd5BTo2t1GQ1SFqJvb7jYaf54uNtftW9lMLg" 
+        alt="" />
+        <h3>Banking App</h3>
+        </div>
+        <div className="item">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn: ANd9GcQWZxqJ65p2YnxZ1Ddd5BTo2t1GQ1SFqJvb7jYaf54uNtftW9lMLg" 
+        alt="" />
+        <h3>Banking App</h3>
+        </div>
+        <div className="item">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn: ANd9GcQWZxqJ65p2YnxZ1Ddd5BTo2t1GQ1SFqJvb7jYaf54uNtftW9lMLg" 
+        alt="" />
+        <h3>Banking App</h3>
+        </div>
+        <div className="item">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn: ANd9GcQWZxqJ65p2YnxZ1Ddd5BTo2t1GQ1SFqJvb7jYaf54uNtftW9lMLg" 
+        alt="" />
+        <h3>Banking App</h3>
+        </div>
+      </div>
 
     </div>
   )
